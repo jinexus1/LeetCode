b73c28bf9acc9c0a0720cd24b18f555217cd7184
jinexus1 <ap2929470@gmail.com>
2024-05-02 16:13:00 +0530

    2125 Number of Laser Beams in a Bank

---
+++ NumberofLaserBeamsinaBank.java
@@ -0,0 +1,19 @@
+//Multiplying two adjacent row will give u the number of connections
+class Solution {
+  public int numberOfBeams(String[] bank) {
+    int total = 0;
+    int prev = 0;
+    for (String row : bank) {
+      int curr = 0;
+      for (int i = 0; i < row.length(); i++) {
+        if (row.charAt(i) == '1')
+          curr++;
+      }
+      if (curr > 0) {
+        total += (prev * curr);
+        prev = curr;
+      }
+    }
+    return total;
+  }
+}
