fddd6fe4c13c635552df5680bb3296b67b653f49
jinexus1 <ap2929470@gmail.com>
2024-07-10 20:32:01 +0530

    Leetcode-152

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -14,3 +14,21 @@ class Solution {
     return (int) max;
   }
 }
+//Approach-2(Observation)
+class Solution {
+  public int maxProduct(int[] nums) {
+    double prefix = 1, suffix = 1;
+    int max = Integer.MIN_VALUE;
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      if (prefix == 0)
+        prefix = 1;
+      if (suffix == 0)
+        suffix = 1;
+      prefix = prefix * (double) nums[i];
+      suffix = suffix * (double) nums[n - i - 1];
+      max = (int) Math.max(Math.max(prefix, suffix), max);
+    }
+    return (int) max;
+  }
+}
