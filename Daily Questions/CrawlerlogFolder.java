b7241dfbd358c52c84d50014b40c669a0a08fa9a
jinexus1 <ap2929470@gmail.com>
2024-07-10 16:07:53 +0530

    Leetcode-1598 (10/6/2024)

--- Daily Questions/CrawlerlogFolder.java
+++ Daily Questions/CrawlerlogFolder.java
@@ -16,3 +16,20 @@ class Solution {
         return counter;
     }
 }
+
+// Approach-2 (Using Stack)
+class Solution {
+  public int minOperations(String[] logs) {
+    Stack<String> st = new Stack<>();
+    for (String log : logs) {
+      if (log.equals("../")) {
+        if (!st.isEmpty()) {
+          st.pop();
+        }
+      } else if (!log.equals("./")) {
+        st.push(log);
+      }
+    }
+    return st.size();
+  }
+}
