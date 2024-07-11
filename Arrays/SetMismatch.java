8d0dfba124e36dc1f491d389c3981bd60bcb5ee4
jinexus1 <ap2929470@gmail.com>
2024-07-11 11:25:55 +0530

    Leetcode-645

--- Arrays/SetMismatch.java
+++ Arrays/SetMismatch.java
@@ -1,3 +1,4 @@
+//Approach-1
 class Solution {
   public int[] findErrorNums(int[] nums) {
     int[] result = new int[2];
@@ -18,5 +19,35 @@ class Solution {
     }
 
     return result;
+  }
+}
+//Approach-2
+class Solution {
+  public int[] findErrorNums(int[] nums) {
+    int n = nums.length;
+    long sum_n = n * (n + 1) / 2; // Sum of first n natural numbers
+    long square_sum_n = n * (n + 1) * (2 * n + 1) / 6; // Sum of squares of first n natural numbers
+
+    long sum_nums = 0;
+    long square_sum_nums = 0;
+
+    for (int num : nums) {
+      sum_nums += num;
+      square_sum_nums += (long) num * (long) num;
+    }
+
+    long diff_sum = sum_nums - sum_n; // x - y
+    long diff_square_sum = square_sum_nums - square_sum_n; // x^2 - y^2
+
+    // From the equations:
+    // x - y = diff_sum
+    // x^2 - y^2 = diff_square_sum = (x - y) * (x + y)
+    // We can derive:
+    long sum_xy = diff_square_sum / diff_sum; // x + y
+
+    int x = (int) ((diff_sum + sum_xy) / 2); // duplicate number
+    int y = (int) (sum_xy - x); // missing number
+
+    return new int[] { x, y };
   }
 }
